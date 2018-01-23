f327a510b3ed2676f6c9bc1ab535ef698280e8d7
Frank <Frank@hotmail.com>
2018-01-23 15:59:25 +0800

    做好介面跟停班停課的資訊頁面

--- app/src/main/java/com/frandog/informationsystemofcompositedisaster/Main4Activity.java
+++ app/src/main/java/com/frandog/informationsystemofcompositedisaster/Main4Activity.java
@@ -1,19 +1,39 @@
 package com.frandog.informationsystemofcompositedisaster;
 
-import android.net.http.SslError;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.SslErrorHandler;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.frandog.informationsystemofcompositedisaster.Fragment.BlankFragment;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class Main4Activity extends AppCompatActivity {
     Spinner sp3;
-    WebView wb;
+    Main4_MyAdapter adapter;
+    Main4_MyHandler dataHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,14 +52,74 @@ public class Main4Activity extends AppCompatActivity {
 
         sp3.setAdapter(adapter);
 
-        wb = findViewById(R.id.webview2);
-        wb.setWebViewClient(new WebViewClient(){
+        sp3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i)
+                {
+                    case 1:
+                        FragmentTransaction ft = getFragmentManager().beginTransaction();
+                        ft.replace(R.id.layout2,new BlankFragment());
+                        ft.commit();
+
+                    //抓網站資料
+                    String str_url = "http://opendata2.epa.gov.tw/AQI.xml";
+                    URL url = null;
+                        try {
+                            url = new URL(str_url);
+                            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                            conn.setRequestMethod("GET");
+                            conn.connect();
+                            InputStream inputStream = conn.getInputStream();
+                            InputStreamReader isr = new InputStreamReader(inputStream);
+                            BufferedReader br = new BufferedReader(isr);
+                            StringBuilder sb = new StringBuilder();
+                            String str;
+                            while ((str = br.readLine()) != null)
+                            {
+                                sb.append(str);
+                            }
+                            String str1 = sb.toString();
+                            dataHandler = new Main4_MyHandler();
+                            SAXParserFactory spf = SAXParserFactory.newInstance();      //本三行為固定寫法
+                            SAXParser sp = spf.newSAXParser();
+                            XMLReader xr = sp.getXMLReader();
+                            xr.setContentHandler(dataHandler);
+                            xr.parse(new InputSource(new StringReader(str1)));
+
+                            br.close();
+                            isr.close();
+                            inputStream.close();
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run(){
+//                                    adapter = new MyAdapter(MainActivity.this,
+//                                            dataHandler.newsItems);
+//                                    lv.setAdapter(adapter);
+                                }
+                            });
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (ParserConfigurationException e) {
+                            e.printStackTrace();
+                        } catch (SAXException e) {
+                            e.printStackTrace();
+                        } catch (ProtocolException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
-        wb.getSettings().setJavaScriptEnabled(true);
-        wb.loadUrl("https://taqm.epa.gov.tw/taqm/aqi-map.aspx");
+
     }
 }
